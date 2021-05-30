0a66bba530a42a1425b9ae60bcf84dcf6d25a315
Yi Wen <ywen.github@gmail.com>
2021-05-30 14:13:25 -0700

    Add deployment

--- src/AuthenticatedArea.tsx
+++ src/AuthenticatedArea.tsx
@@ -2,20 +2,23 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import UserType from 'UserType';
+import Menu from 'Menu';
+
+import 'AuthenticatedArea.scss'
 
 const AuthenticatedArea = ({ user }: UserType) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   useEffect(() => {
-    if (user) {
-      navigate('/home');
-    } else {
+    if (!user) {
       navigate('/signin');
     }
   }, [user, dispatch, navigate]);
   return (
     <div>
-      Test
+      <header className='header'>
+        <Menu />
+      </header>
     </div>
   );
 };
--- src/Menu/index.tsx
+++ src/Menu/index.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+
+import './index.scss';
 const Menu = () => {
   return (
-    <ul>
-      <li><Link to='/home'>Home</Link></li>
-      <li><Link to='/settings'>Settings</Link></li>
+    <ul className='menu__container'>
+      <li className='menu__item'><Link to='/home'>Home</Link></li>
+      <li className='menu__item'><Link to='/settings'>Settings</Link></li>
     </ul>
   )
 };
