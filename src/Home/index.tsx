f101c4213a062a09794cc1cc3aad64957857078c
Yi Wen <ywen.github@gmail.com>
2021-09-05 21:49:22 -0700

    Start on Home

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,7 +1,25 @@
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
+
+import ChoreType from 'commons/ChoreType';
+import ButtonList from 'Components/ButtonList';
+
 const Home = () => {
-  return(
-    <div>Home</div>
-  )
+  const chores: ChoreType[] = useSelector((state: RootState) => state.chores);
+
+  const buttons = [
+    { link: '/settings/chores/add', label: 'Add', buttonClass: 'manage-chores__add' },
+  ];
+  return (
+    <div>
+      <ul className='chores-list__container'>
+        {chores.map(chore => (
+          <li className='chores-list__chore' key={chore.id}>{chore.name} - {chore.points}</li>
+        ))}
+      </ul>
+      <ButtonList buttons={buttons} />
+    </div>
+  );
 };
 
 export default Home;
